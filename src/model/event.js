57e1570b9e65bcefab3be74c251569f1db0de98f
Carter Landis <31522931+ccarterlandis@users.noreply.github.com>
2017-10-29 10:50:41 -0500

    tristan sucks

--- src/model/event.js
+++ src/model/event.js
@@ -5,9 +5,10 @@ const Schema = mongoose.Schema;
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const Event = new Schema({
-  eventDescription: String, default: "",
   eventName: String, default: "",
-  dateTime: {type: Date, default: Date.now},
+  eventDescription: String, default: "",
+  eventLocation: String, default: "",
+  eventDate: {type: Date, default: Date.now},
   userId: {type: ObjectId, ref: 'User'}
 });
 
